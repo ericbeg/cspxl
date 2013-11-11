adbebfe1b3a292a65934bddbe038bb4144afeaad
Eric Begue <ericbeg@gmail.com>
2013-11-11 19:39:29 +0100

    - Added FrameBufferObject skeleton class.

---
+++ opengl/GLFrameBufferObject.cs
@@ -0,0 +1,23 @@
+﻿#pragma  warning disable 1591
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace pxl
+{
+    class GLFrameBufferObject : FrameBufferObject
+    {
+        public GLFrameBufferObject(int width, int height, int depth)
+            : base( width, height, depth)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void AttachColorTexture(Texture tex, int attachment)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
